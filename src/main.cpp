0a9c5ba0407c6811a7c1f406fb3f49e0343465df
canozo <canwoahzo@gmail.com>
2019-05-19 03:46:34 -0600

    shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include "part_table.h"
@@ -7,29 +10,13 @@
 
 using std::cin;
 using std::cout;
+using std::stringstream;
+using std::vector;
+using std::string;
 
-void print_file_info(dir_entry_t *entry) {
-    switch(entry->filename[0]) {
-    case 0x00:
-        return; // unused entry
-    case 0xE5:
-        printf("Deleted file: [?%.7s.%.3s]\n", entry->filename+1, entry->ext);
-        return;
-    case 0x05:
-        printf("File starting with 0xE5: [%c%.7s.%.3s]\n", 0xE5, entry->filename+1, entry->ext);
-        break;
-    case 0x2E:
-        printf("Directory: [%.8s.%.3s]\n", entry->filename, entry->ext);
-        break;
-    default:
-        printf("File: [%.8s.%.3s]\n", entry->filename, entry->ext);
-    }
-
-    printf("  Modified: %04d-%02d-%02d %02d:%02d.%02d    Start: [%04X]    Size: %d\n",
-        1980 + (entry->modify_date >> 9), (entry->modify_date >> 5) & 0xF, entry->modify_date & 0x1F,
-        (entry->modify_time >> 11), (entry->modify_time >> 5) & 0x3F, entry->modify_time & 0x1F,
-        entry->starting_cluster, entry->file_size);
-}
+void shell(FILE *, boot_sector_t *);
+vector<string> split(string);
+bool validate(vector<string>);
 
 int main(int argc, char *argv[]) {
   FILE *file;
@@ -71,15 +58,112 @@ int main(int argc, char *argv[]) {
   // leer la data del boot sector
   fread(&bs, sizeof(boot_sector_t), 1, file);
 
+  shell(file, &bs);
+
   long int offset = (bs.reserved_sectors - 1 + bs.fat_size_sectors * bs.number_of_fats) * bs.sector_size;
   fseek(file, offset, SEEK_CUR);
 
   dir_entry_t entry;
   for (int j = 0; j < bs.root_dir_entries; j += 1) {
     fread(&entry, sizeof(entry), 1, file);
-    print_file_info(&entry);
+    // print_file_info(&entry);
   }
 
   fclose(file);
   return 0;
+}
+
+void shell(FILE *file, boot_sector_t *bs) {
+  string command;
+  vector<string> commands;
+
+  while (true) {
+    cout << "$ ";
+    getline(cin, command);
+    commands = split(command);
+
+    // salir si ingreso "exit"
+    if (commands[0] == "exit") {
+      break;
+    }
+
+    // validar sintaxis correcta y ejecutar
+    if (validate(commands)) {
+      if (commands[0] == "ls") {
+        // ls -l
+        // imprime todos los metadatos de los archivos en el directorio actual
+        // imprime los archivos del directorio raíz
+      } else if (commands[0] == "cat") {
+        if (commands.size() == 2) {
+          // cat archivo
+          // imprime los datos del archivo de texto
+        } else {
+          // cat > archivo.txt
+          // crea el archivo a.txt en el directorio actual
+        }
+      } else if (commands[0] == "mkdir") {
+        // mkdir DIR
+        // crea el directorio DIR en el directorio actual
+      } else if (commands[0] == "cd") {
+        // cd DIR
+        // se mueve al directorio DIR y este es el directorio actual
+      } else {
+        // ???
+        cout << "??? inesperado\n";
+      }
+    }
+  }
+}
+
+vector<string> split(string command) {
+  string buffer;
+  stringstream ss(command);
+  vector<string> res;
+
+  while (ss >> buffer) {
+    res.push_back(buffer);
+  }
+
+  return res;
+}
+
+bool validate(vector<string> commands) {
+  // verificar que ingreso un comando valido
+  if (commands.size() < 2) {
+    cout << "Ingreso muy pocos argumentos.\n";
+    return false;
+  }
+
+  if (commands.size() > 3) {
+    cout << "Ingreso muchos argumentos.\n";
+    return false;
+  }
+
+  // verificar primer comando
+  if (commands[0] != "ls" && commands[0] != "cat" && commands[0] != "mkdir" && commands[0] != "cd") {
+    cout << "Argumento \"" << commands[0] << "\" no valido.\n";
+    return false;
+  }
+
+  // ls -l
+  if (commands[0] == "ls" && commands[1] != "-l") {
+    cout << "Argumento \"" << commands[1] << "\" no valido.\n";
+    return false;
+  }
+
+  // cat > archivo.txt
+  if (commands.size() == 3) {
+    if (commands[0] != "cat") {
+      cout << "Muchos argumentos para \"" << commands[0] << "\".\n";
+      return false;
+    }
+
+    if (commands[1] != ">") {
+      cout << "Argumento \"" << commands[1] << "\" no valido.\n";
+      return false;
+    }
+  }
+
+  // sintaxis correcta
+  return true;
 }
